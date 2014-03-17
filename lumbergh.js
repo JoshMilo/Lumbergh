dde6377356767c56537ab3c9d20fb121cca8e4fb
Josh Milo <josh@joshmilo.com>
2014-03-17 16:45:40 -0700

    added ccslint

--- lumbergh.js
+++ lumbergh.js
@@ -1,1 +1,13 @@
 // QA specific gulpfile
+
+var gulp = require('gulp'),
+    gulputil = require('gulp-util'),
+    csslint = require('gulp-csslint')
+    notify = require('gulp-notify');
+
+gulp.task('wildstyle', function() {
+  gulp.src('build/css/*.css')
+    .pipe(csslint())
+    .pipe(csslint.reporter())
+    .pipe(notify("All right, all right, all right! CSS busted"));
+});
