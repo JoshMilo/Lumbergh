31b72e553270ab887db22c3550a0ccc8e4885a53
Josh Milo <josh@joshmilo.com>
2014-03-18 14:09:32 -0700

    added htmlhint

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 // QA specific gulpfile
 
 var gulp = require('gulp'),
-    gulputil = require('gulp-util'),
-    csslint = require('gulp-csslint')
-    notify = require('gulp-notify');
+    csslint = require('gulp-csslint'),
+    htmlhint = require('gulp-htmlhint'),
+    notify = require('gulp-notify'),
+    gulputil = require('gulp-util');
 
 gulp.task('wildstyle', function() {
   gulp.src('build/css/*.css')
     .pipe(csslint())
     .pipe(csslint.reporter())
     .pipe(notify("All right, all right, all right! CSS busted"));
+});
+
+gulp.task('markymark', function() {
+  gulp.src('build/*.html')
+    .pipe(htmlhint())
+    .pipe(htmlhint.reporter())
+    .pipe(notify("The Funky Bunch has hinted."));
 });
