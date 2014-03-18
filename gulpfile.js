7118e3c302d53ba6bd18831580b4cf959844c29f
Josh Milo <josh@joshmilo.com>
2014-03-18 15:54:20 -0700

    updated csscss, added debug log to gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 // QA specific gulpfile
 
 var gulp = require('gulp'),
+    csscss = require('gulp-csscss'),
     csslint = require('gulp-csslint'),
     htmlhint = require('gulp-htmlhint'),
     notify = require('gulp-notify'),
     gulputil = require('gulp-util');
+
+
+gulp.task('markymark', function() {
+  gulp.src('build/*.html')
+    .pipe(htmlhint())
+    .pipe(htmlhint.reporter())
+    .pipe(notify("The Funky Bunch has hinted."));
+});
 
 gulp.task('wildstyle', function() {
   gulp.src('build/css/*.css')
@@ -13,9 +22,8 @@ gulp.task('wildstyle', function() {
     .pipe(notify("All right, all right, all right! CSS busted"));
 });
 
-gulp.task('markymark', function() {
-  gulp.src('build/*.html')
-    .pipe(htmlhint())
-    .pipe(htmlhint.reporter())
-    .pipe(notify("The Funky Bunch has hinted."));
+gulp.task('dstyle', function() {
+  gulp.src('build/css/*.css')
+    .pipe(csscss())
+    .pipe(gulp.dest('doubles.css'));
 });
