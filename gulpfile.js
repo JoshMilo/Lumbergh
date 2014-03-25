42c8b0f05c9de25ed9aceeadc08c8874bf3c2fc5
Matthew Meyers <matthew.gregory.meyers@gmail.com>
2014-03-25 12:07:20 -0700

    adds reports index generator

--- gulpfile.js
+++ gulpfile.js
@@ -10,10 +10,12 @@ var gulp = require('gulp'),
     size = require('gulp-size'),
     uncss = require('gulp-uncss'),
     w3cjs = require('gulp-w3cjs'),
-    gulputil = require('gulp-util');
+    gulputil = require('gulp-util'),
+    clean = require('gulp-clean');
 
 var fs = require('fs'),
-    mkdirp = require('mkdirp');
+    mkdirp = require('mkdirp'),
+    runSequence = require('run-sequence');
 
 var reportsDir = './reports/json'
 
@@ -26,7 +28,7 @@ gulp.task('markymark', function() {
     }
   };
 
-  gulp.src('build/*.html')
+  return gulp.src('build/*.html')
     .pipe(plumber())
     .pipe(htmlhint())
     .pipe(htmlhint.reporter(jsonReporter))
@@ -42,7 +44,7 @@ gulp.task('wildstyle', function() {
     }
   };
 
-  gulp.src('build/css/*.css')
+  return gulp.src('build/css/*.css')
     .pipe(plumber())
     .pipe(csslint())
     .pipe(csslint.reporter(jsonReporter))
@@ -50,13 +52,13 @@ gulp.task('wildstyle', function() {
 });
 
 gulp.task('validation', function() {
-  gulp.src('build/*.html')
+  return gulp.src('build/*.html')
     .pipe(w3cjs())
     .pipe(notify("Validation Complete!"));
 });
 
 gulp.task('uncss', function() {
-  gulp.src('build/css/*.css')
+  return gulp.src('build/css/*.css')
     .pipe(plumber())
     .pipe(uncss({
       html: ['build/index.html']
@@ -66,7 +68,7 @@ gulp.task('uncss', function() {
 });
 
 gulp.task('cleanhtml', function() {
-  gulp.src('build/*.html')
+  return gulp.src('build/*.html')
     .pipe(plumber())
     .pipe(cleanhtml())
     .pipe(gulp.dest('build/linted'))
@@ -74,7 +76,7 @@ gulp.task('cleanhtml', function() {
 });
 
 gulp.task('size', function() {
-  gulp.src('build')
+  return gulp.src('build')
     .pipe(size(showFiles));
 });
 
@@ -89,8 +91,36 @@ gulp.task('jshint', function() {
     fs.writeFileSync(reportsDir + '/jshint-' + filename + '.json', JSON.stringify(output, null, 2));
   };
 
-  gulp.src('build/js/*.js')
+  return gulp.src('build/js/*.js')
     .pipe(jshint('build/js/.jshintrc'))
     .pipe(jshint.reporter(jsonReporter))
     .pipe(notify('JS has been hinted'));
+});
+
+gulp.task('clean-reports', function () {
+  return gulp.src('./reports/json')
+    .pipe(clean());
+});
+
+gulp.task('reports-index', function (cb) {
+  var reports = fs.readdirSync('./reports/json/');
+  var output = {
+    css: [],
+    html: [],
+    js: []
+  };
+
+  for (var i = reports.length - 1; i >= 0; i--) {
+    if (/^csshint/.test(reports[i])) output.css.push(reports[i]);
+    if (/^jshint/.test(reports[i])) output.js.push(reports[i]);
+    if (/^htmlhint/.test(reports[i])) output.html.push(reports[i]);
+  };
+
+  fs.writeFileSync(reportsDir + '/index.json', JSON.stringify(output, null, 2));
+
+  cb();
+});
+
+gulp.task('generate-reports', function (cb) {
+  runSequence('clean-reports', ['jshint', 'wildstyle', 'markymark'], 'reports-index', cb);
 });
